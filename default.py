04b03b6a127e74a4385bd99a4b4662032a6ff921
doko-desuka <31810999+doko-desuka@users.noreply.github.com>
2020-02-27 00:56:04 +0000

    1.0.4

--- default.py
+++ default.py
@@ -1,211 +1,211 @@
 # -*- coding: utf-8 -*-
+# Order Favourites program add-on for Kodi 17.6+.
+# Lets you see and reorder your Kodi favourites, to organize them.
+#
+# doko-desuka 2020
+# ====================================================================
 import sys
-import os
-import urlparse, urllib
-import json
-import xml.etree.ElementTree as ET
-import xbmc,xbmcaddon,xbmcgui,xbmcplugin,xbmcvfs
+import re
+try:
+    # Python 2.x
+    from HTMLParser import HTMLParser
+    PARSER = HTMLParser()
+except ImportError:
+    # Python 3.4+ (see https://stackoverflow.com/a/2360639)
+    import html
+    PARSER = html    
 
-__handle__ = int(sys.argv[1])
+import xbmc, xbmcaddon, xbmcgui, xbmcvfs
 
-FAVOURITES_PATH = xbmc.translatePath('special://userdata/%s' % 'favourites.xml').decode('utf-8')
-THUMBNAILS_PATH = xbmc.translatePath('special://thumbnails').decode('utf-8')
+FAVOURITES_PATH = 'special://userdata/favourites.xml'
+THUMBNAILS_PATH_FORMAT = 'special://thumbnails/{folder}/{file}'
 
-FAVS_PROP = 'orderfavourites.favourites'
-PAD_PROP = 'orderfavourites.padding'
+# Custom Favourites window class for managing the favourites items.
+class CustomFavouritesDialog(xbmcgui.WindowXMLDialog):
+    def __init__(self, *args, **kwargs):
+        xbmcgui.WindowXMLDialog.__init__(self, *args, **kwargs)
 
-#===================================================================================
+        # Map control IDs to custom handler methods. You can find the control IDs inside
+        # the custom skin XML bundled with this add-on (/resources/skins/Default/1080i/CustomFavouritesDialog.XML).
+        self.idHandlerDict = {
+            101: self.doSelect,
+            301: self.doSave,
+            302: self.doReload,
+            303: self.doClose
+        }
 
-def viewFavourites(params):
-    global __handle__
-    global FAVS_PROP
+        # Map action IDs to handler custom methods, see more action IDs in
+        # https://github.com/xbmc/xbmc/blob/master/xbmc/input/actions/ActionIDs.h
+        self.actionHandlerDict = {
+            # All click/select actions are already handled by 'idHandlerDict' above.
+            #7: self.doSelect, # ACTION_SELECT_ITEM
+            #100: self.doSelect, # ACTION_MOUSE_LEFT_CLICK
+            #108: self.doSelect, # ACTION_MOUSE_LONG_CLICK
+            9: self.doUnselectClose, # ACTION_PARENT_DIR
+            92: self.doUnselectClose, # ACTION_NAV_BACK
+            10: self.doUnselectClose, # ACTION_PREVIOUS_MENU
+            101: self.doUnselectClose, # ACTION_MOUSE_RIGHT_CLICK
+            110: self.doUnselectClose # ACTION_BACKSPACE
+        }
+        self.noop = lambda: None
 
-    if not params:
-        params = { }
-    indexFrom = int( params.get('from', [-1])[0] )
-    indexTo = int( params.get('to', [-1])[0] )
 
-    favs = getWindowProperty(FAVS_PROP)
-    if not favs:
-        favs = loadFavs()
+    # Function used to start the dialog.
+    def doCustomModal(self, favouritesGen, saveHandler):
+        allItems = [ ]
+        artDict = {'thumb': None}
+        for index, data in enumerate(favouritesGen):
+            # The path of each ListItem contains the original favourite entry XML text (with the label, thumb and URL)
+            # and this is what's written to the favourites file upon saving -- what changes is the order of the items.
+            li = xbmcgui.ListItem(data[0], path=data[2])
+            artDict['thumb'] = data[1] # Slightly faster than recreating a dict on every item.
+            li.setArt(artDict)
+            li.setProperty('index', str(index)) # To help with resetting, if necessary.
+            allItems.append(li)
+            
+        self.indexFrom = None # Integer index of the source item (or None when nothing is selected).
+        self.allItems = allItems        
+        self.saveHandler = saveHandler
+        
+        self.doModal()
 
-    listItems = [ ]
-    for index, f in enumerate(favs):
-        li = xbmcgui.ListItem( '[COLOR yellow][B]'+f['name']+'[/B][/COLOR]' if index == indexFrom else f['name'] )
-        cache = f['cache']
-        li.setArt( { 'poster': cache, 'icon': cache } )
 
-        if indexFrom != -1:
-            urlParams = {'from': indexFrom, 'to': index, 'insert': 1}
+    # Automatically called before the dialog is shown, the UI controls exist now.
+    def onInit(self):
+        self.panel = self.getControl(101)
+        self.panel.reset()
+        self.panel.addItems(self.allItems)
+        self.setFocusId(100) # Focus the group containing the panel.
+
+
+    def onClick(self, controlId):
+        self.idHandlerDict.get(controlId, self.noop)()
+
+
+    def onAction(self, action):
+        self.actionHandlerDict.get(action.getId(), self.noop)()
+
+
+    def doSelect(self):
+        selectedPosition = self.panel.getSelectedPosition()
+        if self.indexFrom == None:
+            # Selecting a new item to reorder.
+            self.indexFrom = selectedPosition
+            self.panel.getSelectedItem().setProperty('selected', '1')
         else:
-            urlParams = {'from': index, 'to': -1, 'reroute': 1}
-        url = buildUrl(urlParams)
-        listItems.append( (url, li, False) )
-
-    liSave = xbmcgui.ListItem('[COLOR greenyellow]SAVE[/COLOR]')
-    saveUrl = buildUrl( {'save':1} )
-    listItems.append( (saveUrl, liSave, False) )
-
-    liReset = xbmcgui.ListItem('[COLOR hotpink]RESET[/COLOR]')
-    resetUrl = buildUrl( {'reset':1} )
-    listItems.append( (resetUrl, liReset, False) )
-
-    # Add dummy items as padding so the grid shows 4 per row like the Favourites screen.
-    if getWindowProperty(PAD_PROP) > 1:
-        alignedItems = [ ('', xbmcgui.ListItem(''), False) for n in range(3) ] + listItems
-    else:
-        alignedItems = listItems # Custom skins, we can't safely use padding items.
-
-    xbmcplugin.addDirectoryItems(__handle__, alignedItems)
-    xbmcplugin.endOfDirectory(__handle__)
-    xbmc.executebuiltin('Container.SetViewMode(500)') # Estuary skin, grid mode.
-    if indexFrom != -1:
-        selectWindowIndex( indexFrom )
-    elif indexTo != -1:
-        selectWindowIndex( indexTo )
+            # Something was already selected.
+            if self.indexFrom != selectedPosition:
+                # User selected a different item, so consider the new index as the destination.
+                itemFrom = self.allItems.pop(self.indexFrom)
+                self.allItems.insert(selectedPosition, itemFrom)     
+                
+                # Reset the selection state.
+                self.indexFrom = None
+                itemFrom.setProperty('selected', '')
+                
+                # Update the panel.
+                self.panel.reset()
+                self.panel.addItems(self.allItems)
+                self.panel.selectItem(selectedPosition)
+            else: # User reselected the item, so just unmark it.
+                self.indexFrom = None
+                self.panel.getSelectedItem().setProperty('selected', '')                        
 
 
-def viewSave():
-    global FAVS_PROP
-    global FAVOURITES_PATH
-    dialog = xbmcgui.Dialog()
-    dialog.notification( 'Order Favourites', 'Saving...',
-                            xbmcgui.NOTIFICATION_INFO, 2000, False ) # No sound.
-    favs = getWindowProperty(FAVS_PROP)
-    if favs:
-        data = '<favourites>' + ''.join( [ f['original'] for f in favs ] ) + '</favourites>'
-        tree = ET.fromstring( data )
-        treeIndent( tree )
-        file = xbmcvfs.File(FAVOURITES_PATH, 'w')
-        file.write( ET.tostring( tree, encoding='utf-8') )
-        file.close()
-        clearWindowProperty(FAVS_PROP)
-        clearWindowProperty(PAD_PROP)
-        xbmc.sleep(100) # Just to give the I/O some time.
-        profileName = xbmc.getInfoLabel('System.ProfileName')
-        xbmc.executebuiltin('LoadProfile(%s)' % profileName) # Reloads 'favourites.xml'.
+    def doUnselectClose(self):
+        # If there's something selected, unselect it. Otherwise, close the dialog.
+        if self.indexFrom != None:
+            self.allItems[self.indexFrom].setProperty('selected', '')
+            self.indexFrom = None
+        else:
+            self.doClose()           
+            
+            
+    def doClose(self):
+        xbmc.executebuiltin('ActivateWindow(home)') # Return to the Home screen (https://kodi.wiki/view/Window_IDs).
+        self.close()
+            
+
+    def doReload(self):
+        if xbmcgui.Dialog().yesno(
+            'Order Favourites',
+            'This will undo any of your changes and restore the order from your Favourites file.\nProceed?'
+        ):
+            # Re-sort all items based on their original indices.
+            self.indexFrom = None        
+            self.allItems = sorted(self.allItems, key=lambda li: int(li.getProperty('index')))        
+            self.panel.reset()        
+            self.panel.addItems(self.allItems)        
 
 
-def viewInsert(params):
-    indexFrom = int( params.get('from', [-1])[0] )
-    indexTo = int( params.get('to', [-1])[0] )
-    if indexFrom != -1 and indexTo != -1 and indexFrom != indexTo:
-        global FAVS_PROP
-        favs = getWindowProperty(FAVS_PROP)
-        if favs:
-            # New method, reinsert.
-            favs.insert( indexTo, favs.pop( indexFrom ) )
-            setWindowProperty( FAVS_PROP,  favs )
-    url = buildUrl( {'from': -1, 'to': indexTo} ) # 'indexTo' is set so it can be auto-selected.
-    xbmc.executebuiltin('Container.Update(%s,replace)' % url)
+    def doSave(self):
+        if self.saveHandler(self.allItems):
+            self.close()
+            xbmcgui.Dialog().ok('Order Favourites', 'Save complete. Reloading home screen now...')
+            # Reload the current profile (which causes a reload of 'favourites.xml').
+            xbmc.executebuiltin('LoadProfile(%s)' % xbmc.getInfoLabel('System.ProfileName'))
+        else:
+            xbmcgui.Dialog().notification(
+                'Order Favourites',
+                'Could not save the favourites file',
+                xbmcgui.NOTIFICATION_WARNING,
+                3000,
+                False
+            )
 
 
-def viewReset():
-    loadFavs()
-    url = buildUrl( {'from': -1} )
-    xbmc.executebuiltin('Container.Update(%s,replace)' % url)
-
-
-# This is needed to bypass Kodi's URL history behaviour
-# since those ListItems are not folders.
-def viewReroute(params):
-    indexFrom = params.get('from', [-1])[0]
-    indexTo = params.get('to', [-1])[0]
-    url = buildUrl( {'from':indexFrom, 'to':indexTo} )
-    xbmc.executebuiltin('Container.Update(%s,replace)' % url)
-
-#===================================================================================
-
-def buildUrl(query):
-    return sys.argv[0] + '?' + urllib.urlencode(query)
-
-
-def getWindowProperty(prop):
-    currentWindow = xbmcgui.Window(xbmcgui.getCurrentWindowId())
-    data = currentWindow.getProperty(prop)
-    return json.loads(data) if data else { }
-
-
-def setWindowProperty(prop, data):
-    currentWindow = xbmcgui.Window( xbmcgui.getCurrentWindowId())
-    temp = json.dumps(data)
-    currentWindow.setProperty(prop, temp)
-    return temp
-
-
-def clearWindowProperty(prop):
-    currentWindow = xbmcgui.Window( xbmcgui.getCurrentWindowId())
-    currentWindow.clearProperty(prop)
-
-
-def selectWindowIndex( index ):
-    currentWindow = xbmcgui.Window( xbmcgui.getCurrentWindowId() )
-    padding = currentWindow.getProperty(PAD_PROP)
-    index += int( padding ) if padding else 0
-    cID = currentWindow.getFocusId()
-    try:
-        currentWindow.getControl(cID).selectItem(index)
-    except:
-        pass
-
-
-def loadFavs():
-    global FAVOURITES_PATH
-    global THUMBNAILS_PATH
-    global FAVS_PROP
-    global PAD_PROP
-
+def favouritesDataGen():
     file = xbmcvfs.File(FAVOURITES_PATH)
-    tree = ET.fromstring(file.read())
+    contents = file.read()
     file.close()
-    favs = [ ]
-    for f in tree.iter('favourite'):
-        thumb = f.get('thumb', '')
-        cache = xbmc.getCacheThumbName(thumb).replace('.tbn', '.jpg')
-        path = THUMBNAILS_PATH + '%s/%s' % (cache[0], cache)
-        cache = thumb if ( cache.startswith('ffffffff') or not xbmcvfs.exists(path) ) else path
-        favs.append( {'name': f.get('name', ''), 'cache': cache, 'original': ET.tostring(f, encoding='utf-8')} )
-    setWindowProperty(FAVS_PROP, favs)
 
-    # Padding, helps with the auto-selection.
-    rpcQuery = { 'jsonrpc': '2.0', 'id': '1', 'method': 'Settings.GetSettingValue',
-                           'params': {'setting': 'filelists.showparentdiritems'} }
-    r = json.loads( xbmc.executeJSONRPC( json.dumps(rpcQuery) ) )
-    padding = 1 if r['result'].get('value', False) else 0
-    padding += 3 if padding and xbmc.getSkinDir().endswith('estuary') else 0 # For the dummy items we add on Estuary.
-    setWindowProperty(PAD_PROP, padding)
+    namePattern = re.compile('name="([^"]+)')
+    thumbPattern = re.compile('thumb="([^"]+)')
 
-    return favs
+    for entryMatch in re.finditer('(<favourite\s[^<]+</favourite>)', contents):
+        entry = entryMatch.group(1)
+        
+        match = namePattern.search(entry)
+        name = PARSER.unescape(match.group(1)) if match else ''
+        
+        match = thumbPattern.search(entry)
+        if match:
+            thumb = PARSER.unescape(match.group(1))
+            cacheFilename = xbmc.getCacheThumbName(thumb)            
+            if 'ffffffff' not in cacheFilename:
+                if '.jpg' in thumb:
+                    cacheFilename = cacheFilename.replace('.tbn', '.jpg', 1)
+                if '.png' in thumb:
+                    cacheFilename = cacheFilename.replace('.tbn', '.png', 1)
+                thumb = THUMBNAILS_PATH_FORMAT.format(folder=cacheFilename[0], file=cacheFilename)
+        else:
+            thumb = ''
+            
+        # Yield a 3-tuple of name, thumb-url and original favourites entry content.
+        yield name, thumb, entry
 
 
-# From http://effbot.org/zone/element-lib.htm#prettyprint
-def treeIndent(elem, level=0):
-    i = '\n' + level * '    '
-    if len(elem):
-        if not elem.text or not elem.text.strip():
-            elem.text = i + '    '
-        if not elem.tail or not elem.tail.strip():
-            elem.tail = i
-        for elem in elem:
-            treeIndent(elem, level + 1)
-        if not elem.tail or not elem.tail.strip():
-            elem.tail = i
-    else:
-        if level and (not elem.tail or not elem.tail.strip()):
-            elem.tail = i
+def doSave(allItems):
+    # allItems = list of ListItems in the order they should be. Their 'path' holds the XML text of each item.
+    INDENT_STRING = ' ' * 4
+    xmlText = '<favourites>\n' + '\n'.join((INDENT_STRING + li.getPath()) for li in allItems) + '</favourites>\n'
+    try:
+        file = xbmcvfs.File(FAVOURITES_PATH, 'w')
+        file.write(xmlText)
+        file.close()
+    except:
+        return False
+    return True
+
+
+# Debugging helper. Logs a LOGNOTICE-level message.
+def xbmcLog(*args):
+    xbmc.log('LOG > ' + ' '.join((var if isinstance(var, str) else repr(var)) for var in args), xbmc.LOGNOTICE)
 
 #===================================================================================
 
 ### Entry point ###
 
-params = urlparse.parse_qs(sys.argv[2][1:])
-if 'save' in params:
-    viewSave()
-elif 'insert' in params:
-    viewInsert(params)
-elif 'reset' in params:
-    viewReset()
-elif 'reroute' in params:
-    viewReroute(params)
-else:
-    viewFavourites(params)
+ui = CustomFavouritesDialog('CustomFavouritesDialog.xml', xbmcaddon.Addon().getAddonInfo('path'))
+ui.doCustomModal(favouritesDataGen(), doSave)
+del ui # Delete the dialog instance after it's done, as it's not garbage collected.
